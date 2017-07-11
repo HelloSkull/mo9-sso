0aa506054e83ba8d8096906c0dd8d62cf878eba8
caozh <zihaocao@mo9.com>
2017-07-11 16:57:19 +0800

    升级版本0.1.2

--- README.md
+++ README.md
@@ -14,42 +14,58 @@ const Mo9SSO = require("mo9-sso");
 const mo9SSO = new Mo9SSO();
 
 ...
- const opt = {
-            url,
-            userInfo
+       const queryParams = {
+            url:`${config.VALIDATE_INTERFACE_SERVICE}/get_user_privileges`,
+            queryParams: {
+                userId,
+                interface,
+                projectId
+            },
         };
 
- const resData =  await mo9SSO.judgeTicket(opt);
+ const resData =  mo9SSO.judgeTicket(queryParams);
 
 ```
 请求参数和返回格式
 
 **request**
 ``` javascript
-    url:远程获取ticket信息的请求地址
-    userInfo:存储在session中的userInfo对象
+    url//远程获取ticket信息的请求地址
+    userId//当前用户的id
+    interface//接口名称
+    projectId//项目的id
 ```
 
 **response**
 ``` javascript
-  {
+    {
         "code" : {
             "type" : "integer",
             "description" : "报错号,0表示没有错误，1041表示登录失效"
         },
         "data" : {
-            "description" : "session对象"
+            "description" : "返回信息"
             "type" : "object",
             "properties":{
-                "userInfo":{
-                    "description" : "用户信息",
-                    "type" : "object"
-                 }
+                code: 0,
+                  message: null,
+                  data: {
+                    entity: {
+                           privileges: [
+                             '/jerboa/article/back_add',
+                             '/jerboa/article/back_get_page',
+                             '/jerboa/author/back_get_id',
+                             '/jerboa/article/back_update_id',
+                             '/jerboa/author/back_get_page',
+                             '/jerboa/article/back_batch_delete',
+                             '/jerboa/author/back_update_id',
+                             '/jerboa/article/back_get_id',
+                             '/jerboa/author/back_add',
+                             '/jerboa/author/back_batch_delete'
+                           ]
+                     }
+                }
             }
-        }
-        "message" : {
-            "type" : "string",
-            "description" : "错误信息,如果未出错,则此字段没值"
         }
    }
 ```
--- index.js
+++ index.js
@@ -1,53 +1,49 @@
 var fetch = require ('node-fetch');
 // 存储session和获取session方式由使用者去实现，这里只用作判断
 function mo9SSO(){
-    this.judgeTicket = function (opt){
-        var url = opt.url;
-        // var userInfo = opt.userInfo;
-        let data = { code:0 };
-        // if(!userInfo){
+    this.judgeTicket = function (params){
         return new Promise((resolve,reject)=>{
-            this.getTicketPromise(url).then(function(resData){
-                const { resultData,session } =  resData;
-                //如果返回有数据的话就设置cookie
-                if(resultData) {
-                    data = {
-                        code:0,
-                        data:{ session }
-                    };
-                }else{
-                    data = {
-                        code:1041,
-                        data:null,
-                        message:"登录失效，即将跳转到登录页面！"
-                    };
-                }
-                console.log("getTicketPromise data->",data);
-                resolve(data) ;
+            this.getUserPrivileges(params).then(function(resultData){
+                console.log("resultData ->",resultData);
+                const  privileges  = resultData.data ?　resultData.data.entity.privileges : null;
+                const { interface } = params.queryParams;
+                console.log("privileges ->",privileges);
+                console.log("interface ->",interface);
+                let flag = false;
+                privileges && privileges.forEach((v,i)=>{
+                    if(v === interface){
+                        flag = true;
+                        return false;
+                    }
+                });
+                console.log("flag ->",flag);
+                resultData.data = flag;
 
+                resolve(resultData);
             }).then(function(error){
                 resolve ({ code:-1,error })
             })
         });
-        /*   }else{
-         return  data;
-         }*/
     };
-    this.getTicketPromise = function(url){
+    this.getUserPrivileges = function(params){
         return new Promise((resolve,reject)=>{
-            const params = {
-                url
-            };
+
+            if (params.queryParams) {
+                params.url = `${params.url}?${(() => {
+                    let queryParamsStr = '';
+                    for (const i in params.queryParams) {
+                        if (params.queryParams[i] || params.queryParams[i] === 0) {
+                            queryParamsStr += `${i}=${params.queryParams[i]}&`;
+                        }
+                    }
+                    return queryParamsStr.substring(0, queryParamsStr.length - 1);
+                })()}`;
+            }
+
             fetch(params.url)
                 .then(res => res.json())
                 .then(data => {
-                    const obj = {
-                        resultData:data,
-                        session:{
-                            userInfo:data
-                        }
-                    };
-                    resolve(obj);
+                    resolve(data);
                 }).then(err=>{
                 reject(err);
             })
