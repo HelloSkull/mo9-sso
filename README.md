af40d657ca906541ce591d46dc9663f9f1567ef3
caozh <zihaocao@mo9.com>
2017-09-26 16:51:08 +0800

    修改验证机制，升级到第二版本

--- README.md
+++ README.md
@@ -10,30 +10,34 @@
 使用方法:
 
 ``` javascript
-const Mo9SSO = require("mo9-sso");
-const mo9SSO = new Mo9SSO();
-
-...
-       const queryParams = {
-            url:`${config.VALIDATE_INTERFACE_SERVICE}/get_user_privileges`,
-            queryParams: {
+    mo9SSODealData: async (param, ctx) => {
+        console.log('param ->', param);
+        const { ticket, userId, interface } = param;
+        const params = {
+            interfaces: config.INTERFACES,
+            params: {
                 userId,
-                interface,
-                projectId
-            },
-        };
-
- const resData =  mo9SSO.judgeTicket(queryParams);
+                ticket,
+                ip: ctx.request.host,
+                accessKeyId: config.PROJECT_ACCESS_KEY_ID,
+                accessKeySecret: config.PROJECT_ACCESS_KEY_SECRET,
+                code: interface
+            }
+        }
+        return mo9SSO.judge(params);
+    }
 
 ```
 请求参数和返回格式
 
 **request**
 ``` javascript
-    url//远程获取ticket信息的请求地址
-    userId//当前用户的id
-    interface//接口名称
-    projectId//项目的id
+    userId//用户的id
+    ticket//用户的ticket
+    ip//ip地址
+    accessKeyId//项目的accessKeyId，具体请登录大后台（bk.mo9.com）看
+    accessKeySecret//项目的accessKeySecret，具体请登录大后台（bk.mo9.com）看
+    code//接口的权限码
 ```
 
 **response**
@@ -41,30 +45,13 @@ const mo9SSO = new Mo9SSO();
     {
         "code" : {
             "type" : "integer",
-            "description" : "报错号,0表示没有错误，1041表示登录失效"
+            "description" : "报错号,0表示没有错误，11080001表示没有权限"
         },
         "data" : {
             "description" : "返回信息"
             "type" : "object",
             "properties":{
-                code: 0,
-                  message: null,
-                  data: {
-                    entity: {
-                           privileges: [
-                             '/jerboa/article/back_add',
-                             '/jerboa/article/back_get_page',
-                             '/jerboa/author/back_get_id',
-                             '/jerboa/article/back_update_id',
-                             '/jerboa/author/back_get_page',
-                             '/jerboa/article/back_batch_delete',
-                             '/jerboa/author/back_update_id',
-                             '/jerboa/article/back_get_id',
-                             '/jerboa/author/back_add',
-                             '/jerboa/author/back_batch_delete'
-                           ]
-                     }
-                }
+                code: 0
             }
         }
    }
--- index.js
+++ index.js
@@ -1,54 +1,134 @@
-var fetch = require ('node-fetch');
+var fetch = require('node-fetch');
+const crypto = require('crypto');
 // 存储session和获取session方式由使用者去实现，这里只用作判断
-function mo9SSO(){
-    this.judgeTicket = function (params){
-        return new Promise((resolve,reject)=>{
-            this.getUserPrivileges(params).then(function(resultData){
-                console.log("resultData ->",resultData);
-                const  privileges  = resultData.data ?　resultData.data.entity.privileges : null;
-                const { interface } = params.queryParams;
-                console.log("privileges ->",privileges);
-                console.log("interface ->",interface);
-                let flag = false;
-                privileges && privileges.forEach((v,i)=>{
-                    if(v === interface){
-                        flag = true;
-                        return false;
-                    }
-                });
-                console.log("flag ->",flag);
-                resultData.data = flag;
+function mo9SSO() {
 
-                resolve(resultData);
-            }).then(function(error){
-                resolve ({ code:-1,error })
-            })
-        });
-    };
-    this.getUserPrivileges = function(params){
-        return new Promise((resolve,reject)=>{
+    this.judge = function (param) {
+        const { interfaces, params } = param;
+        const { ticket, ip, accessKeyId, accessKeySecret, code, userId } = params;
+        // console.log('userId ->', userId);
+        // console.log('interfaces ->', interfaces);
+        // console.log('params ->', params);
+        // console.log('ticket->', ticket);
+        // console.log('accessKeyId ->', accessKeyId);
 
-            if (params.queryParams) {
-                params.url = `${params.url}?${(() => {
-                    let queryParamsStr = '';
-                    for (const i in params.queryParams) {
-                        if (params.queryParams[i] || params.queryParams[i] === 0) {
-                            queryParamsStr += `${i}=${params.queryParams[i]}&`;
+        const identityAuthenticationParams = {
+            url: interfaces.identityAuthentication,
+            type: 0,
+            queryParams: { ticket, userId },
+            accessKeySecret,
+            accessKeyId
+        }
+
+        const securityAuthenticationParams = {
+            url: interfaces.securityAuthentication,
+            type: 0,
+            queryParams: { ip, accessKeyId },
+            accessKeySecret,
+            accessKeyId
+        }
+
+        const authorizationAuthenticationParams = {
+            url: interfaces.authorizationAuthentication,
+            type: 0,
+            queryParams: { code, userId, accessKeyId },
+            accessKeySecret,
+            accessKeyId
+        }
+        const identityAuthentication = this.fetchFunc(identityAuthenticationParams);
+        const securityAuthentication = this.fetchFunc(securityAuthenticationParams);
+        const authorizationAuthentication = this.fetchFunc(authorizationAuthenticationParams);
+
+        return Promise.all([identityAuthentication, securityAuthentication, authorizationAuthentication]).then(result => {
+            let backData = {};
+            console.log('result ->', result);
+            //三个条件全部满足才能通过
+            if (result[0].code === 0 && result[1].code === 0 && result[2].code === 0) {
+                backData = {
+                    code: 0,
+                    data: []
+                }
+            } else {
+                backData = {
+                    code: 11020001,
+                }
+            }
+            return backData;
+        })
+
+    }
+
+    this.fetchFunc = function (params) {
+        const { queryParams, formParams, accessKeySecret, accessKeyId, type } = params;
+        const Timestamp = new Date().getTime();
+        let Content = "";
+        let Signature = "";
+        const P = { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8' };
+
+        return new Promise((resolve, reject) => {
+            if (type !== 0) {
+                P.method = "POST";
+                const formParamArr = [];
+                if (formParams) {
+                    for (const i in formParams) {
+                        if (this.judgeHasKey(i, formParams)) {
+                            formParamArr.push(`${i}=${this.dealValue(formParams[i])}`);
                         }
                     }
-                    return queryParamsStr.substring(0, queryParamsStr.length - 1);
+                }
+                Content = formParamArr.join('&');
+                return formParamArr.join('&');
+            } else {
+                P.method = "GET";
+                const queryParamArr = [];
+                params.url = `${params.url}?${(() => {
+                    for (const i in queryParams) {
+                        if (this.judgeHasKey(i, queryParams)) {
+                            queryParamArr.push(`${i}=${this.dealValue(queryParams[i])}`);
+                        }
+                    }
+                    Content = queryParamArr.join('&');
+                    return queryParamArr.join('&');
                 })()}`;
             }
 
-            fetch(params.url)
+            Signature = this.md5(`${Content}${Timestamp}${accessKeySecret}`);
+            P.headers = {
+                Timestamp,
+                Signature,
+                ["Access-Key-Id"]: accessKeyId
+            }
+            // console.log('params.url->', params.url);
+            // console.log('P->', P);
+
+            fetch(params.url, P)
                 .then(res => res.json())
                 .then(data => {
                     resolve(data);
-                }).then(err=>{
-                reject(err);
-            })
+                }).then(err => {
+                    reject(err);
+                })
         })
-    };
+    }
+
+    this.judgeHasKey = function (key, obj) {
+        return Object.prototype.hasOwnProperty.call(obj, key) && obj.hasOwnProperty(key) !== 'undefined';
+    }
+
+    this.dealValue = function (value) {
+        // 如果是字符串，去掉前后空格
+        if (Object.prototype.toString.call(value) === '[object String]') {
+            return value.replace(/(^\s*)|(\s*$)/g, '');
+        } else {
+            return value;
+        }
+    },
+        this.md5 = function (encryptString) {
+            const hasher = crypto.createHash("md5");
+            hasher.update(encryptString);
+            encryptString = hasher.digest('hex');
+            return encryptString;
+        }
 }
 
 module.exports = mo9SSO;
